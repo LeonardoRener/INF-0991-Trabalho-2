c244d1205b1243e0df3409ba2cac11c122c007c6
lrener <leonardo.rener@mc1.com.br>
2022-09-05 10:18:55 -0300

    V1.0

--- Game/Coordinate.cs
+++ Game/Coordinate.cs
@@ -19,22 +19,6 @@ public class Coordinate
         return this.pos_y;
     }
 
-    public bool IsAdjacentPosition(Coordinate coordinate)
-    {
-        bool result = false;
-
-        if (coordinate.Compare(this.pos_x+1, this.pos_y))
-            result = true;
-        else if (coordinate.Compare(this.pos_x, this.pos_y+1))
-            result = true;
-        else if (coordinate.Compare(this.pos_x-1, this.pos_y))
-            result = true;
-        else if (coordinate.Compare(this.pos_x, this.pos_y-1))
-            result = true;
-
-        return result;
-    }
-
     private bool Compare(int x, int y)
     {
         return this.pos_x == x && this.pos_y == y;
